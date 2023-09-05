cb622a05888f8ab6b253f0f8eef1a0703e2689a1
Gabriel Pedutti <gabrielpedutti@hotmail.com>
2023-09-05 15:22:15 -0300

    Adicionado tabela dinamica da lista dos processos atuais

--- script.js
+++ script.js
@@ -1,26 +1,59 @@
-const btnImpressora = document.querySelector('#btnImpressora')
-const btnFoto = document.querySelector('#btnFoto')
+document.addEventListener("DOMContentLoaded", function () {
+const btnPrinter = document.querySelector('#btnPrinter')
+const btnPhoto = document.querySelector('#btnPhoto')
 const btnGithub = document.querySelector('#btnGithub')
 const btnEmail = document.querySelector('#btnEmail')
+const tableBody = document.querySelector('tbody')
 
-const statusProcesso = {
-    pronto: "pronto",
-    executando: "executando",
-    terminado: "terminado"
+const statusProcess = {
+    ready: "ready",
+    running: "running",
+    finished: "finished"
 }
 
-class Processo {
-    constructor(burst, arrivalTime, id) {
+let globalTime = 0;
+
+class Process {
+    constructor(name, burst, arrivalTime, id) {
+        this.name = name;
         this.pID = id;
         this.burst = burst;
-        this.status = statusProcesso.pronto;
+        this.status = statusProcess.ready;
         this.waitTime = 0;
         this.executionTime = 0;
         this.arrivalTime = arrivalTime;
     }
-
-
 }
   
-let processo = new Processo(1,0,0);
-  
+let processSystem = new Process("System", 100 , 0, 0);
+let processHost = new Process("Host", 10, 1, 1);
+let processSecurity = new Process("Security", 15, 2, 2);
+let processRegistry = new Process("Registry", 18, 3, 3);
+
+let pcb = [processSystem, processHost, processSecurity, processRegistry];
+
+class Scheduler {
+    constructor(quantum, pcb) {
+        this.quantum = quantum;
+        this.pcb = pcb;
+    }
+
+    updateTable() {
+        tableBody.innerHTML = "";
+        pcb.forEach(process => {
+            tableBody.innerHTML += `
+                <tr>
+                    <td>${process.name}</td>
+                    <td>${process.burst}</td>
+                    <td>${process.executionTime}</td>
+                </tr>
+            `
+        });
+    }
+    
+}
+
+let scheduler = new Scheduler(6, pcb);
+scheduler.updateTable()
+
+})
