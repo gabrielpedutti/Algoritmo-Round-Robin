a1efa0855636594d8ee9e887e594e9b220f1e4ae
tsansalone <41164166+tsansalone@users.noreply.github.com>
2023-09-06 11:46:58 -0300

    Mudanças feitas em aula 05/09/23 Correção de bugs e implementação da feature scaling

--- script.js
+++ script.js
@@ -1,71 +1,129 @@
 document.addEventListener("DOMContentLoaded", function () {
-const btnPrinter = document.querySelector('#btnPrinter')
-const btnPhoto = document.querySelector('#btnPhoto')
-const btnGithub = document.querySelector('#btnGithub')
-const btnEmail = document.querySelector('#btnEmail')
-const tableBody = document.querySelector('tbody')
-
-const statusProcess = {
-    ready: "ready",
-    running: "running",
-    finished: "finished"
-}
-
-let globalTime = 0;
-
-class Process {
-    constructor(name, burst, arrivalTime, id) {
-        this.name = name;
-        this.pID = id;
-        this.burst = burst;
-        this.status = statusProcess.ready;
-        this.waitTime = 0;
-        this.executionTime = 0;
-        this.arrivalTime = arrivalTime;
+    const btnPrinter = document.querySelector('#btnPrinter')
+    const btnPhoto = document.querySelector('#btnPhoto')
+    const btnGithub = document.querySelector('#btnGithub')
+    const btnEmail = document.querySelector('#btnEmail')
+    const tableBody = document.querySelector('tbody')
+    const tbodyProcessFinished = document.querySelector('#tbodyProcessFinished')
+    
+    //remainingTime = Tempo restante até o término do processo
+    //Burst = Tempo total do processo (fixo)
+    //ExecutionTime = Quanto tempo já executou
+    
+    const statusProcess = {
+        ready: "ready",
+        running: "running",
+        finished: "finished"
     }
-}
-  
-let processSystem = new Process("System", 100 , 0, 0);
-let processHost = new Process("Host", 10, 1, 1);
-let processSecurity = new Process("Security", 15, 2, 2);
-let processRegistry = new Process("Registry", 18, 3, 3);
-
-let pcb = [processSystem, processHost, processSecurity, processRegistry];
-
-class Scheduler {
-    constructor(quantum, pcb) {
-        this.quantum = quantum;
-        this.pcb = pcb;
+    
+    finishedProcesses = [];
+    
+    const rotate = (arr) => {
+        let a = [arr[arr.length - 1]];
+        for (let i = 0; i < arr.length - 1; i++) {
+          a.push(arr[i]);
+        }
+        return a;
+      };
+    
+    let globalTime = 0;
+    
+    class Process {
+        constructor(name, burst, arrivalTime, id) {
+            this.name = name;
+            this.pID = id;
+            this.burst = burst;
+            this.status = statusProcess.ready;
+            this.waitTime = 0;
+            this.executionTime = 0;
+            this.arrivalTime = arrivalTime;
+            this.remainingTime = burst;
+        }
     }
-
-    updateTable() {
-        tableBody.innerHTML = "";
-        pcb.forEach(process => {
-            tableBody.innerHTML += `
-                <tr>
-                    <td>${process.name}</td>
-                    <td>${process.burst}</td>
-                    <td>${process.executionTime}</td>
-                </tr>
-            `
-        });
-    }
-
-    scaling() {
-        if(pcb.length > 0) {
-            if(pcb[0].burst > 0) {
-                pcb[0].status = statusProcess.ready;
-                pcb[0].executionTime += this.quantum;
-                for (let i = 1; i < array.length; i++) {
-                    pcb[i].waitTime += this.quantum;                    
+      
+    let processSystem = new Process("System", 100 , 0, 0);
+    let processHost = new Process("Host", 10, 1, 1);
+    let processSecurity = new Process("Security", 15, 2, 2);
+    let processRegistry = new Process("Registry", 18, 3, 3);
+    
+    let processControlBlock = [processSystem, processHost, processSecurity, processRegistry];
+    
+    class Scheduler {
+        constructor(quantum, processControlBlock) {
+            this.quantum = quantum;
+            this.processControlBlock = processControlBlock;
+        }
+    
+        updateTable() {
+            tableBody.innerHTML = "";
+            processControlBlock.forEach(process => {
+                tableBody.innerHTML += `
+                    <tr>
+                        <td>${process.name}</td>
+                        <td>${process.burst}</td>
+                        <td>${process.executionTime}</td>
+                        <td>${process.remainingTime}</td>
+                        <td>${process.waitTime}</td>
+                    </tr>
+                `
+            });
+    
+            tbodyProcessFinished.innerHTML = "";
+            finishedProcesses.forEach(process => {
+                tbodyProcessFinished.innerHTML += `
+                    <tr>
+                        <td>${process.name}</td>
+                        <td>${process.burst}</td>
+                        <td>${process.executionTime}</td>
+                        <td>${process.remainingTime}</td>
+                        <td>${process.waitTime}</td>
+                    </tr>
+                `
+            });
+        }
+    
+        scaling() {
+            if(processControlBlock.length > 0) {
+                if(processControlBlock[0].remainingTime >= this.quantum) {
+                    processControlBlock[0].status = statusProcess.running;
+                    processControlBlock[0].executionTime += this.quantum;
+                    processControlBlock[0].remainingTime -= this.quantum;
+                    for (let i = 1; i < processControlBlock.length; i++) {
+                        processControlBlock[i].waitTime += this.quantum;                    
+                    }
+                    if (processControlBlock[0].remainingTime == 0) {
+                        processControlBlock[0].status = statusProcess.finished;
+                    } else {
+                        processControlBlock[0].status = statusProcess.ready;
+                    }
+                    globalTime += this.quantum;
+                    this.updateTable()
+                    processControlBlock = rotate(processControlBlock)
+                } else {
+                    globalTime += processControlBlock[0].remainingTime;
+                    processControlBlock[0].status = statusProcess.running;
+                    processControlBlock[0].executionTime += processControlBlock[0].remainingTime;
+                    processControlBlock[0].remainingTime = 0;
+                    for (let i = 1; i < processControlBlock.length; i++) {
+                        processControlBlock[i].waitTime += this.quantum;                    
+                    }
+                        processControlBlock[0].status = statusProcess.finished;
+                    
+                        this.updateTable()
+                        finishedProcesses.push(processControlBlock[0]);
+                        processControlBlock.shift()
                 }
-                globalTime += this.quantum;
             }
         }
     }
-}
-
-let scheduler = new Scheduler(6, pcb);
-scheduler.updateTable()
-
-})
+    
+    
+    
+    let scheduler = new Scheduler(6, processControlBlock);
+    
+    scheduler.updateTable()
+    for (let i = 0; i < 100; i++) {
+        setTimeout(scheduler.scaling(), 5000);
+    }
+    
+    })
