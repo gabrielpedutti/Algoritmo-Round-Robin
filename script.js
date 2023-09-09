62f2b984ee91c04fdbdbccb2ffd34d2faae6dc53
Gabriel Pedutti <gabrielpedutti@hotmail.com>
2023-09-09 01:52:09 -0300

    Correção bug pulando 1 passo

--- script.js
+++ script.js
@@ -135,7 +135,6 @@ document.addEventListener("DOMContentLoaded", function () {
             }
             this.updateTable()
             this.updateTableFinishedProcess()
-            updateChart()
         }
     }
     
